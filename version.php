2a2aa8a67399007741869244642f2c354639cee9
dprieler <dprieler@gmail.com>
2016-09-21 11:17:20 +0200

    bump version#

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015121701;		// The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016092100;		// The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014050800;		// Requires this Moodle version
 $plugin->component = 'block_exa2fa';	   // Full name of the plugin (used for diagnostics)
-$plugin->release = 'v1.0';
+$plugin->release = '4.5.0';
